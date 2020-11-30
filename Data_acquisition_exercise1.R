5bdf5a9802d7c6e2d78876ea899a99e84541fbb7
Asish kumar ghosh <ruet.asish@gmail.com>
2020-12-01 01:55:32 +0600

    first commit

--- Data_acquisition_exercise1.R
+++ Data_acquisition_exercise1.R
@@ -11,8 +11,9 @@ Sys.setenv(SPOTIFY_CLIENT_SECRET = '307841367f1c4457baea58d6bf6584ff')
 
 access_token <- get_spotify_access_token()
 
-beatles <- get_artist_audio_features('the beatles')%>% 
-  head(30)
+beatles <- get_artist_audio_features('the beatles')%>%
+  select(artist_name,artist_id, album_id,album_type, album_release_date, album_release_year, album_release_date_precision, danceability,artists ,track_uri,external_urls.spotify , album_name, key_name, mode_name ,key_mode)%>%
+  head(5)
 
 beatles
 
